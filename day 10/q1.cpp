75d5f9f9281e7d44d832faad59f77f7c7e8b07f6
pari7maheshwari <parimaheshwari777@gmail.com>
2026-06-10 21:39:51 +0530

    Added day 10

---
+++ day 10/q1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number of rows : ";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=n;j>=i;j--)
+        {
+            cout<<" ";
+        }
+        for(int j=1;j<=i*2-1;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
